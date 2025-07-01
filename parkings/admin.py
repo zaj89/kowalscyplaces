a603b46fb9b300e5c2d29f2dfd0e9c42869caca7
zajac <zajac89@o2.pl>
2025-07-01 22:04:38 +0200

    przeniesienie modelu Parking do aplikacji Locations

--- parkings/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from parkings.models import Parking
-
-
-@admin.register(Parking)
-class ParkingAdmin(admin.ModelAdmin):
-    list_display = ('name', 'coords', 'description')
-    search_fields = ('name',)
--- parkings/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('mapa/', views.parking_map, name='parking_map'),
-    path('mapa/add/', views.add_parking, name='add_parking'),
-]
